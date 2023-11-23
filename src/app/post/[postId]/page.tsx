d113fb184ae9c62f157a6623f6314c4fe79cd3a3
Peter Lucido <127632779+PeterLucido@users.noreply.github.com>
2023-11-23 14:39:12 -0600

    adjuster

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -1,75 +1,75 @@
-'use client';
-import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+// 'use client';
+// import { usePathname } from 'next/navigation';
+// import { useEffect, useState } from 'react';
 
-// Define a type for the fetched data
-interface PostData {
-  id: number;
-  name: string;
-  createdAt: string;
-  updatedAt: string;
-  createdById: string;
-  // Add other properties if needed
-}
+// // Define a type for the fetched data
+// interface PostData {
+//   id: number;
+//   name: string;
+//   createdAt: string;
+//   updatedAt: string;
+//   createdById: string;
+//   // Add other properties if needed
+// }
 
-function PostDetail() {
-  const pathname = usePathname();
-  const segments = pathname.split('/');
-  const postIdIndex = segments.findIndex(s => s === 'post') + 1;
-  const postId = segments[postIdIndex];
-  console.log("postId:", postId)
+// function PostDetail() {
+//   const pathname = usePathname();
+//   const segments = pathname.split('/');
+//   const postIdIndex = segments.findIndex(s => s === 'post') + 1;
+//   const postId = segments[postIdIndex];
+//   console.log("postId:", postId)
   
-  const [post, setPost] = useState<PostData | null>(null);
+//   const [post, setPost] = useState<PostData | null>(null);
   
-  useEffect(() => {
-    console.log("postId:", postId); // Log postId before fetching
+//   useEffect(() => {
+//     console.log("postId:", postId); // Log postId before fetching
   
-    const fetchPost = async () => {
-      try {
-        const fetchUrl = `/api/post?id=${postId}`;
-        console.log("Fetching Post:", postId);
-        console.log("Fetch URL:", fetchUrl);
+//     const fetchPost = async () => {
+//       try {
+//         const fetchUrl = `/api/post?id=${postId}`;
+//         console.log("Fetching Post:", postId);
+//         console.log("Fetch URL:", fetchUrl);
   
-        const response = await fetch(fetchUrl);
+//         const response = await fetch(fetchUrl);
   
-        if (response.ok) {
-          console.log("Response OK");
-          const postData = await response.json() as PostData | undefined;
+//         if (response.ok) {
+//           console.log("Response OK");
+//           const postData = await response.json() as PostData | undefined;
   
-          if (postData) {
-            console.log("Response Data:", postData);
-            setPost(postData); // Log before and after setting post state
-          } else {
-            console.error("Invalid data structure in response");
-          }
-        } else {
-          console.error("Error fetching post:", response.status);
-        }
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-    void fetchPost();
+//           if (postData) {
+//             console.log("Response Data:", postData);
+//             setPost(postData); // Log before and after setting post state
+//           } else {
+//             console.error("Invalid data structure in response");
+//           }
+//         } else {
+//           console.error("Error fetching post:", response.status);
+//         }
+//       } catch (error) {
+//         console.error('Error:', error);
+//       }
+//     };
+//     void fetchPost();
   
-    console.log("After calling fetchPost, post is:", post); // Log after fetchPost is called
-  }, [post, postId]);
+//     console.log("After calling fetchPost, post is:", post); // Log after fetchPost is called
+//   }, [post, postId]);
   
   
-  console.log(888888, post);
-  console.log("Post State:", post);
+//   console.log(888888, post);
+//   console.log("Post State:", post);
 
-  if (post === null) { // Change this condition to explicitly check for null
-    console.log("post is null");
-    return <div>Loading...</div>;
-  }
+//   if (post === null) { // Change this condition to explicitly check for null
+//     console.log("post is null");
+//     return <div>Loading...</div>;
+//   }
 
-  console.log("Rendering Post Detail");
+//   console.log("Rendering Post Detail");
 
-  return (
-    <div>
-      <h1>Post Name: {post.name}</h1>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Post Name: {post.name}</h1>
+//     </div>
+//   );
+// }
 
-export default PostDetail;
+// export default PostDetail;
